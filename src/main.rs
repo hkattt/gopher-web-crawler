b23051bd0c2649472df29fc63f204a0f44a1c532
hkattt <hugo.kat@outlook.com>
2024-04-11 18:00:01 +1000

    Connected to local host and anu gopher server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{Read, Write};
+use std::net::TcpStream;
+
+fn main() -> std::io::Result<()> {
+    // Open server on Gophie with: comp3310.ddns.net:70
+
+    //let mut stream = TcpStream::connect("127.0.0.1:70")?;
+
+    let mut stream = TcpStream::connect("comp3310.ddns.net:70")?;
+
+    let selector = String::from("\r\n");
+    let mut buffer = String::new();
+
+    stream.write(selector.as_bytes())?;
+    stream.read_to_string(&mut buffer)?;
+
+    println!("{buffer}");
+    
+    Ok(())
 }
