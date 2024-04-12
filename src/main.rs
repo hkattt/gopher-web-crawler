8099e67ae773fc2e33dacd55032a7c282571dd07
hkattt <hugo.kat@outlook.com>
2024-04-12 16:26:47 +1000

    Created Crawler and Request structs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,53 @@
-use std::io::{Read, Write};
+use std::io::{Read, Write, Error, ErrorKind};
 use std::net::TcpStream;
-use chrono::format::Item;
 use chrono::{Local, Timelike};
 
 // Open server on Gophie with: comp3310.ddns.net:70
 // Local host: 127.0.0.1
 
 // const BUFFER_SIZE: u32  = 4096; // TODO: Is this needed?
-const CRLF: &str           = "\r\n";
-const TAB: &str            = "\t";
-const COLON: &str          = ":";
-const DOT: &str            = ".";
-const SERVER_DETAILS: &str = "comp3310.ddns.net:70";
+const CRLF: &str              = "\r\n";
+const TAB: &str               = "\t";
+const COLON: &str             = ":";
+const DOT: &str               = ".";
+const STARTING_SELECTOR: &str = "";
+const SERVER_NAME: &str       = "comp3310.ddns.net";
+const SERVER_PORT: u16        = 70;
+
+struct Request<'a> {
+    selector: &'a str, 
+    server_details: String,
+}
+
+impl<'a> Request<'a> {
+    fn new(selector: &'a str, server_name: &'a str, server_port: u16) -> Request<'a> {
+        let server_details = [server_name, COLON, &server_port.to_string()].concat();
+        
+        Request {
+            selector,
+            server_details,
+        }
+    }
+
+    fn send(&self) -> std::io::Result<TcpStream> {
+        // Get the current local time
+        let local_time = Local::now();
+    
+        let server_details = &self.server_details;
+    
+        let mut stream = TcpStream::connect(server_details)?;
+    
+        println!("[{:02}h:{:02}m:{:02}s]: REQUESTING {} FROM {}", 
+            local_time.time().hour(), local_time.time().minute(), local_time.time().second(),
+            self.selector, self.server_details
+        );
+    
+        let selector = [self.selector, CRLF].concat();
+        stream.write(selector.as_bytes())?;
+    
+        Ok(stream)
+    }
+}
 
 enum ItemType {
     TXT,     // 0   Item is a text file
@@ -30,9 +66,11 @@ struct ResponseLine<'a> {
 
 impl<'a> ResponseLine<'a> {
     fn new(line: &'a str) -> Option<ResponseLine<'a>> {
-        println!("Response line: {}", line);
         // TODO: What if there are not 4 tabs? Result NOne
         let mut parts = line.splitn(4, TAB);
+
+        // TODO: Can we do this without cloning?
+        if parts.clone().count() != 4 {return None;}
 
         let user_display_string = parts.next().unwrap();
         
@@ -76,6 +114,7 @@ struct Crawler {
     nerr: u32,           // The number of unique invalid references (error types)
     // TODO: A list of external servers (see spec)
     // TODO: Any references that have "issues/errors" that your code needs to explicity deal with
+    used_selectors: Vec<String>,
 }
 
 impl Default for Crawler {
@@ -92,90 +131,119 @@ impl Default for Crawler {
             largest_bin: 0,
             nerr: 0,
             // TODO: Add to this
+            used_selectors: Vec::new(),
         }
     }
 }
 
 impl Crawler {
-    fn crawl(&mut self, server_details: &str) -> std::io::Result<()> {
-        let mut stream = TcpStream::connect(server_details)?;
-
-        let selector = String::from(CRLF);
+    fn crawl(&mut self, selector: &str, server_name: &str, server_port: u16) -> std::io::Result<()> {
+        let request = Request::new(selector, server_name, server_port);
+        self.used_selectors.push(STARTING_SELECTOR.to_string());
+        
+        let stream = request.send();
         let mut buffer = String::new();
 
-        stream.write(selector.as_bytes())?;
+        let mut stream = match stream {
+            Ok(stream) => stream,
+            Err(error) => {
+                match error.kind() {
+                    _ => panic!("Problem sending request")
+                }
+            }
+        };
         stream.read_to_string(&mut buffer)?;
 
         let lines = buffer.split(CRLF);
 
         for line in lines {
-            self.process_response_line(line)?;
+            match self.process_response_line(line) {
+                Ok(_) => {},
+                Err(error) => {
+                    match error.kind() {
+                        ErrorKind::Other => {},
+                        _ => panic!("Problem processing response line")
+                    }
+                }
+            }
         }
-        
         Ok(())
     }
 
     fn process_response_line(&mut self, line: &str) -> std::io::Result<()> {
         // TODO: Do this better?
         if line.eq(DOT) {
-            println!("Encountered dot");
             return Ok(())
         }
         // TODO: Handle the None case better?
-        let response_line = ResponseLine::new(line).unwrap();
+        let response_line = ResponseLine::new(line);
+        let response_line = match response_line {
+            Some(response_line) => {response_line},
+            None => return Err(Error::new(ErrorKind::Other, "Malformed response line")),
+        };
     
         match response_line.item_type {
             ItemType::TXT => {
+                if self.has_crawled(response_line.selector) { return Ok(()) }
+
+                self.used_selectors.push(response_line.selector.to_string());
+
                 self.ntxt += 1;
-                let stream = send_response_line(response_line);
-                match stream {
-                    Ok(stream) => {
-                        //let buffer = recv_response_line(stream)?;
-                        //println!("DOCUMENT:\n{buffer}");
-                    }
-                    Err(e) => eprintln!("Error sending response line: {e}")
-                }
+                // let stream = send_response_line(response_line);
+                // match stream {
+                //     Ok(stream) => {
+                //         let buffer = recv_response_line(stream)?;
+                //         //println!("DOCUMENT:\n{buffer}");
+                //     }
+                //     Err(e) => eprintln!("Error sending response line: {e}")
+                // }
             },
             ItemType::DIR => {
                 self.ndir += 1;
+
+                if response_line.get_server_details() != SERVER_NAME {
+                    // TODO: Handle external servers
+                    // Only need to try connecting
+                }
+
+                if self.has_crawled(response_line.selector) { return Ok(()) }
+                
+                self.used_selectors.push(response_line.selector.to_string());
+
+                self.crawl(response_line.selector, 
+                    response_line.server_name, 
+                    response_line.server_port.parse().unwrap()
+                )?;
             },
             ItemType::ERR => {
                 self.nerr += 1;
+                self.ndir -= 1; // Ignore parent directory that led to the error
             },
             ItemType::BIN => {
+                if self.has_crawled(response_line.selector) { return Ok(()) }
+
+                self.used_selectors.push(response_line.selector.to_string());
+
                 self.nbin += 1;
             },
             ItemType::UNKNOWN => {
                 // TODO: Should we do stuff with this?
+                return Ok(())
             },
         }
         Ok(())
     }
+
+    fn has_crawled(&self, selector: &str) -> bool {
+        self.used_selectors.iter().any(|used_selector| used_selector == selector)
+    }
 }
 
 fn main() -> std::io::Result<()> {
+    // TODO: Should we make SERVER_PORT a &str?
     let mut crawler = Crawler { ..Default::default() };
-    crawler.crawl(SERVER_DETAILS)?;
+    crawler.crawl(STARTING_SELECTOR, SERVER_NAME, SERVER_PORT)?;
     Ok(())
-}
-
-fn send_response_line(response_line: ResponseLine) -> std::io::Result<TcpStream> {
-    // Get the current local time
-    let local_time = Local::now();
-
-    let server_details = response_line.get_server_details();
-
-    let mut stream = TcpStream::connect(server_details)?;
-
-    println!("[{:02}h:{:02}m:{:02}s]: REQUESTING {} FROM {} AT {}", 
-        local_time.time().hour(), local_time.time().minute(), local_time.time().second(),
-        response_line.selector, response_line.server_name, response_line.server_port
-    );
-
-    let selector = [response_line.selector, CRLF].concat();
-    stream.write(selector.as_bytes())?;
-
-    Ok(stream)
 }
 
 fn recv_response_line(mut stream: TcpStream) -> std::io::Result<String> {
